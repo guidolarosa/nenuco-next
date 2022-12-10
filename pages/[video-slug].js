829bcb71c5769a2ab81f01a6ec48ef7899e41daf
guidolarosa <guido.glarosa@gmail.com>
2022-12-10 10:23:27 -0300

    First commit

---
+++ pages/[video-slug].js
@@ -0,0 +1,58 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useState } from 'react';
+import Homepage from '../components/Homepage'
+import { createClient } from '../prismicio';
+
+
+export default function VideoPost(props) {
+  const [selectedVideoPostData, setSelectedVideoPostData] = useState(null);
+  useEffect(() => {
+    const videoPost = props.videoPosts.find((e) => {
+      if (e.uid == props.videoUID) {
+        return true;
+      }
+    });
+    setSelectedVideoPostData(videoPost)
+  }, [props.videoPosts, props.videoUID]);
+
+  return (
+    <Homepage {...props} videoUID={
+        selectedVideoPostData && selectedVideoPostData.data.video_id[0].text}>
+    </Homepage>
+  )
+}
+
+export async function getStaticProps({params}) {
+  const client = createClient()
+  const page = await client.getSingle('homepage');
+  const videoPosts = await client.getAllByType('video-post');
+  return {
+    props: {
+      page: page,
+      videoPosts: videoPosts,
+      videoUID: params['video-slug']
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const client = createClient()
+  const videoPosts = await client.getAllByType('video-post');
+  const paths = [];
+
+  videoPosts.forEach((e) => {
+    paths.push(
+      {
+        params: {
+          'video-slug': e.uid
+        }
+      }
+    )
+  })
+  // paths: [{params: {i}}]
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
+import Link from 'next/link'
+import GlobalStyle from '../styles/globalStyles'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
 
 export default MyApp
